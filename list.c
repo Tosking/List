a792606a023eb5758fc4233fc9eea82381471bbb
Tosking <tosking@duck.com>
2022-09-29 17:37:42 +0500

    deleted data and index storage

--- list.c
+++ list.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 typedef struct Node{
-    int data;
-    int index;
     struct Node *next;
     struct Node *prev;
 } Node;
@@ -21,20 +19,20 @@ void init_list(List *list){
 }
 Node *__find_node(List *list, int index){
     Node *buff = NULL;
+    int i = 0;
     if(index < (list->size / 2)){
-        for(buff = list->head; buff->index != index; buff = buff->next);
+        for(buff = list->head; i != index; buff = buff->next) i++;
         return buff;
     }
     else{
-        for(buff = list->tail; buff->index != index; buff = buff->prev);
+        for(buff = list->tail; i != index; buff = buff->prev) i++;
         return buff;
     }
 }
 
-void add_item(List *list, int value){
+void add_item(List *list){
     Node *item = (Node*)malloc(sizeof(Node));
     if(list->size != 0){
-        item->index = list->tail->index + 1;
         item->prev = list->tail;
         list->tail->next = item;
         list->tail = item;
@@ -42,33 +40,21 @@ void add_item(List *list, int value){
     else{
         list->head = item;
         list->tail = item;
-        item->index = 0;
     }
     list->size++;
     item->next = NULL;
-    item->data = value;
 }
 
 void print_list(List *list){
     if(list->size == 0){
         return;
     }
+    int i = 0;
     Node *buff = list->head;
     printf("List:%p\tHead:%p\tTail:%p\n", list, list->head, list->tail);
-    printf("#\t\tp\t\tprev\t\tnext\n");
+    printf("#\tp\t\tprev\t\tnext\n");
     for(buff = list->head; buff != NULL; buff = buff->next){
-        printf("%d.\t%d\t%p\t%p\t%p\n", buff->index, buff->data, buff, buff->prev, buff->next);
-    }
-}
-
-int get_data(List *list, int index){
-    Node *buff;
-    buff = __find_node(list, index);
-    if(buff != NULL){
-        return buff->data;
-    }
-    else{
-        return 0;
+        printf("%d.\t%p\t%p\t%p\n", i, buff, buff->prev, buff->next);
     }
 }
 
@@ -78,8 +64,6 @@ void remove_item(List *list, int index){
     buff->prev->next = buff->next;
     buff->next->prev = buff->prev;
     list->size--;
-    for(buff = buff->next; buff != NULL; buff = buff->next)
-        buff->index--;
 }
 
 void delete_item(List *list, int index){
@@ -92,12 +76,10 @@ void delete_item(List *list, int index){
 int main(){
     List *list = (List*)malloc(sizeof(List));
     init_list(list);
-    add_item(list, 10);
-    add_item(list, 20);
-    add_item(list, 30);
+    add_item(list);
+    add_item(list);
+    add_item(list);
     delete_item(list, 1);
     print_list(list);
-    int x = get_data(list, 1);
-    printf("%d", x);
     return 0;
 }
